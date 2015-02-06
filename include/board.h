2c1bc3105b63acf805fd9dcd980473e0a7bd885d
Peter Jansweijer <peterj@nikhef.nl>
2015-02-06 11:19:46 +0100

    define refclk period in board.h

--- include/board.h
+++ include/board.h
@@ -12,3 +12,12 @@
 #else
 #  include "board-wrc.h"
 #endif
+
+/* WR Reference clock period (picoseconds) and frequency (Hz) */
+#ifdef SPEC_CLOCK
+    #define REF_CLOCK_PERIOD_PS 8000
+    #define REF_CLOCK_FREQ_HZ 125000000
+#else
+    #define REF_CLOCK_PERIOD_PS 16000
+    #define REF_CLOCK_FREQ_HZ 62500000
+#endif
