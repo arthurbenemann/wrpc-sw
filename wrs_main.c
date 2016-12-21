d2a9c4fdeb06f172251b324575e034c7249a4c71
rizzi <mattia.rizzi@cern.ch>
2016-12-21 15:15:57 +0000

    Added start-up softpll startup text

--- wrs_main.c
+++ wrs_main.c
@@ -13,6 +13,8 @@
 #include "minipc.h"
 #include "revision.h"
 #include "system_checks.h"
+#include "gpio-wrs.h"
+#include "ext-board.h"
 
 
 int scb_ver = 33;		/* SCB version */
@@ -40,6 +42,8 @@ int main(void)
 	      build_revision, build_date, build_time);
 	pp_printf("SCB version: %d. %s\n", scb_ver,(scb_ver>=34)?"10 MHz SMC Output.":"" );
 	pp_printf("Start counter %d\n", stats.start_cnt);
+	if (gpio_in(GPIO_EXT_BOARD_DETECT))
+	  pp_printf("\n--- WRS Low jitter board detected. ---\nAllow 1 hour of warming up before starting measurements\n");
 	pp_printf("--\n");
 
 	if (stats.start_cnt > 1) {
@@ -56,7 +60,7 @@ int main(void)
 	{
 		uint32_t tics = timer_get_tics();
 
-		if (time_after(tics, start_tics + TICS_PER_SECOND/5)) {
+		if (time_after(tics, start_tics + TICS_PER_SECOND/4)) {
 			spll_show_stats();
 			start_tics = tics;
 		}
