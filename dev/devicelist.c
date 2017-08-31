726884c73fac386d8bf37db8934f86fe4b18e168
hongming <lihm.thu@gmail.com>
2017-08-31 16:32:50 +0800

    Remove unused ETHERBONE related para.

--- dev/devicelist.c
+++ dev/devicelist.c
@@ -18,7 +18,6 @@ unsigned char *BASE_PPS_GEN;
 unsigned char *BASE_SYSCON;
 unsigned char *BASE_UART;
 unsigned char *BASE_ONEWIRE;
-unsigned char *BASE_ETHERBONE_CFG;
 unsigned char *BASE_EXT_CFG;
 
 /* The sdb filesystem itself */
@@ -72,7 +71,6 @@ struct wrc_device devs[] = {
 	{&BASE_SYSCON,        VID_CERN,	0xff07fc47},
 	{&BASE_UART,          VID_CERN,	0xe2d13d04},
 	{&BASE_ONEWIRE,       VID_CERN,	0x779c5443},
-	{&BASE_ETHERBONE_CFG, VID_GSI,	0x68202b22},
 	{&BASE_EXT_CFG,       VID_THU,	0xc0413599},
 };
 
--- host/fake-hw.c
+++ host/fake-hw.c
@@ -11,5 +11,5 @@ unsigned char *BASE_PPS_GEN = (void *)&_pps;
 static unsigned char _pll[sizeof(struct SPLL_WB)];
 unsigned char *BASE_SOFTPLL = (void *)&_pll;
 
-static unsigned char _ebone[EB_IPV4 + 4];
-unsigned char *BASE_ETHERBONE_CFG = (void *)&_ebone;
+// static unsigned char _ebone[EB_IPV4 + 4];
+// unsigned char *BASE_ETHERBONE_CFG = (void *)&_ebone;
