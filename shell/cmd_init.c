228c1ac08647f933e6ece30f44bd24b820513244
hongming <lihm.thu@gmail.com>
2017-09-03 17:56:27 +0800

    Add spll reset in command "init boot".
        So that the "init boot" can truely restart the whole software.

--- shell/cmd_init.c
+++ shell/cmd_init.c
@@ -27,6 +27,7 @@ static int cmd_init(const char *args[])
 	} else if (args[0] && !strcasecmp(args[0], "show")) {
 		storage_init_show();
 	} else if (args[0] && !strcasecmp(args[0], "boot")) {
+		spll_very_init();
 		shell_boot_script();
 	}
 
