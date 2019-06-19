1ef2e8a9abf9575ed978c69f2513ac9f197f5703
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2019-06-19 09:13:41 +0200

    dev: added bit-banged driver for AD951x

--- include/dev/ad951x.h
+++ include/dev/ad951x.h
@@ -1,27 +1,34 @@
-#ifndef __AD9510_H
-#define __AD9510_H
+#ifndef __AD951x_H
+#define __AD951x_H
+
+#include <stdint.h>
 
 #include "board.h"
-#include "gpio.h"
-#include "spi.h"
+#include "dev/gpio.h"
+#include "dev/spi.h"
 
-#define BIT_DELAY 100
+#define AD951X_BIT_DELAY 100
 
-
-struct ad95xx_config_reg {
+struct ad951x_config_reg {
     uint16_t addr;
     uint8_t value;
 };
 
-struct ad95xx_config {
+struct ad951x_config {
     int n_regs;
-    struct ad95xx_config_reg regs[];
+    struct ad951x_config_reg regs[];
+};
+
+struct ad951x_device {
+    struct spi_bus *bus;
+    struct gpio_pin *pin_reset;
+    struct gpio_pin *pin_lock;
 };
 
 
-void ad9510_write(struct spi_bus *bus, uint32_t reg, uint32_t value);
-uint32_t ad9510_read(struct spi_bus *bus, uint32_t reg);
-int ad9510_configure(struct spi_bus *bus, struct ad95xx_config *cfg);
-void ad9510_init();
+void ad951x_write(struct ad951x_device *dev, uint32_t reg, uint32_t value);
+uint32_t ad951x_read(struct ad951x_device *dev, uint32_t reg);
+int ad951x_configure(struct ad951x_device *dev, struct ad951x_config *cfg);
+int ad951x_init(struct ad951x_device *dev, struct spi_bus *spi, struct gpio_pin *pin_reset, struct gpio_pin *pin_lock);
 
 #endif
