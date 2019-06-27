bd5f1e638c53004a766c62387cd28a6d6bb6c81a
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2019-06-27 20:23:39 +0200

    dev: initial driver for AD9910

--- dev/ad9910.c
+++ dev/ad9910.c
@@ -16,23 +16,35 @@ uint32_t ad9910_read(struct ad9910_device *dev, uint32_t reg, int nbits)
     return rv;
 }
 
-void ad9910_write(struct ad9910_device *dev, uint32_t reg, uint8_t value)
+void ad9910_write(struct ad9910_device *dev, uint32_t reg, uint32_t value)
 {
-
+    bb_spi_cs(dev->bus, 1);
+    bb_spi_write( dev->bus, reg, 8);
+    bb_spi_write( dev->bus, value, 32);
+    bb_spi_cs(dev->bus, 0);
 }
+
+void ad9910_trigger_update(struct ad9910_device *dev)
+{
+    gen_gpio_out(dev->pin_ioupdate, 1); // acknowledge
+    gen_gpio_out(dev->pin_ioupdate, 0); 
+}
+
+#define AD9910_REG_CFR1 1
+#define AD9910_DEFAULT_CFR1  0x400820
 
 int ad9910_probe( struct ad9910_device *dev, struct spi_bus *bus )
 {
-
-    
     dev->bus = bus;
     bb_spi_cs(dev->bus, 0);
-    
-    for(;;)
-    {
-        uint32_t id = ad9910_read( dev, 1, 32 );
-        pp_printf("AD9910 ID[%p]: 0x%x\n", dev, id );
-    }
 
-    return 0;
+    gen_gpio_out(dev->pin_ioupdate, 0);
+
+    ad9910_write( dev, 0, 0x02000002); // unidir mode for SDIO
+    ad9910_trigger_update( dev );
+
+    uint32_t id = ad9910_read( dev, AD9910_REG_CFR1, 32 );
+    pp_printf("AD9910 ID[%p]: 0x%x (expected 0x%x)\n", dev, id, AD9910_DEFAULT_CFR1 );
+
+    return (id == AD9910_DEFAULT_CFR1) ? 0 : -1;
 }
--- include/dev/ad9910.h
+++ include/dev/ad9910.h
@@ -10,6 +10,7 @@
 
 struct ad9910_device {
     struct spi_bus *bus;
+    struct gpio_pin *pin_ioupdate;
 };
 
 struct ad9910_config_reg {
@@ -18,7 +19,8 @@ struct ad9910_config_reg {
 };
 
 uint32_t ad9910_read(struct ad9910_device *dev, uint32_t reg, int nbits);
-void ad9910_write(struct ad9910_device *dev, uint32_t reg, uint8_t value);
+void ad9910_write(struct ad9910_device *dev, uint32_t reg, uint32_t value);
 int ad9910_probe( struct ad9910_device *dev, struct spi_bus *bus );
+void ad9910_trigger_update(struct ad9910_device *dev);
 
 #endif
