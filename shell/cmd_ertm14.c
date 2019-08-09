036cfd5e6c6fb27afe688d47b142a871ceb15d3f
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2019-08-10 00:28:40 +0200

    shell: updated ertm14 command handler

--- shell/cmd_ertm14.c
+++ shell/cmd_ertm14.c
@@ -23,20 +23,26 @@ static int cmd_ertm(const char *args[])
 	int i;
 
 	if (!strcasecmp(args[0], "ct")) {
+        struct wb_clock_monitor_device* cm = &board.ertm14_cmon;
 		pp_printf("eRTM clock test:\n");
-        wb_cm_restart( &ertm14_cmon );
+        wb_cm_restart( cm );
         usleep(3000000);
-        wb_cm_read( &ertm14_cmon );
+        wb_cm_read( cm );
 
-        for( i = 0; i < ertm14_cmon.n_channels; i++ )
+        for( i = 0; i < cm->n_channels; i++ )
         {
-            if( ertm14_cmon.freq_valid_mask & (1<<i)) 
+            if( cm->freq_valid_mask & (1<<i)) 
             {
-                pp_printf("%d [%s] : %d Hz\n", i, clock_names[i], ertm14_cmon.freqs[i]);
+                pp_printf("%d [%s] : %d Hz\n", i, clock_names[i], cm->freqs[i]);
             }
          }
-    
-	}
+	} else if (!strcasecmp(args[0], "dds")) {
+        
+    } else if (!strcasecmp(args[0], "rf-distr")) {
+        
+    } else if (!strcasecmp(args[0], "clk-distr")) {
+        
+    }
 }
 
 DEFINE_WRC_COMMAND(ertm) = {
