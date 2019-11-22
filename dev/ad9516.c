49b65568906efa1de90764302d6685def3a026a1
Peter Jansweijer <peterj@nikhef.nl>
2019-11-22 16:50:01 +0100

    added system clock source (for lm32) selection to break the AD9519 deadlock

--- dev/ad9516.c
+++ dev/ad9516.c
@@ -251,7 +251,14 @@ int spec7_ad9516_init(int ext_10mhz)
   } else {
     /* Configuration for the SPEC7: Forward 125 MHz VCXO_REFCLK at CLK input to outputs 0, 1, 2 */
     ad9516_load_regset(spi_base, ad9516_base_config_spec7, ARRAY_SIZE(ad9516_base_config_spec7), 0);
-  }    
+  }
+
+	timer_delay(10);
+  pp_printf("Switch clk_sys source from free running clk_dmtd to AD9516 output.\n");
+ 	/* AD9516 now initialized so switch clk_sys from free running clk_dmtd to AD9516 output */
+	gpio_out(GPIO_PLL_CLK_SEL, 1);
+	timer_delay(10);
+
 }
 
 #else /* CONFIG_WR_SWITCH */
