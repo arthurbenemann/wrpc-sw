46d8660ed31ee4a55afd64363e8f8d6a7e4abf53
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-02-21 00:44:56 +0100

    dev: added generic bit-banged SPI & I2C drivers

---
+++ include/dev/bb_i2c.h
@@ -0,0 +1,28 @@
+/*
+ * This work is part of the White Rabbit project
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+#ifndef __I2C_H
+#define __I2C_H
+
+#include <stdint.h>
+
+struct i2c_bus
+{
+    struct gpio_pin *pin_scl;
+    struct gpio_pin *pin_sda;
+    int loop_delay;
+};
+
+uint8_t bb_i2c_devprobe(struct i2c_bus *bus, uint8_t i2c_addr);
+void bb_i2c_create(struct i2c_bus *bus, struct gpio_pin *pin_scl, struct gpio_pin *pin_sda );
+void bb_i2c_init(struct i2c_bus *bus);
+void bb_i2c_start(struct i2c_bus *bus);
+void bb_i2c_repeat_start(struct i2c_bus *bus);
+void bb_i2c_stop(struct i2c_bus *bus);
+void bb_i2c_get_byte(struct i2c_bus *bus, uint8_t *data, uint8_t last);
+uint8_t bb_i2c_put_byte(struct i2c_bus *bus, uint8_t data);
+void bb_i2c_delay(uint32_t delay);
+
+#endif
