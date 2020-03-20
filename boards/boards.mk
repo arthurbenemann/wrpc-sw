d86f7aa7053418f0c2b5463a33e87019e00db4a2
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-03-20 13:54:03 +0100

    boards/ertm14: imported eRTM14 board support files from tom-dev-ertm14 branch and made the eRTM14 BSP

--- boards/boards.mk
+++ boards/boards.mk
@@ -2,3 +2,5 @@ obj-$(CONFIG_TARGET_GENERIC_PHY_8BIT) += boards/generic/board.o
 obj-$(CONFIG_TARGET_GENERIC_PHY_16BIT) += boards/generic/board.o
 obj-$(CONFIG_TARGET_WR_SWITCH) += boards/wr-switch/main.o boards/wr-switch/gpio-wrs.o boards/wr-switch/ad9516.o
 obj-$(CONFIG_TARGET_AFCZ) += boards/afcz/board.o
+obj-$(CONFIG_TARGET_ERTM14) += boards/ertm14/board.o boards/ertm14/dds_sync_unit.o boards/ertm14/ertm15_rf_distr.o boards/ertm14/phy_calibration.o
+
