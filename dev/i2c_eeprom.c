67c140c4276d977d3982517d4b4f6aabdee54c87
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-04-07 00:03:57 +0200

    dev/i2c_eeprom: support for 2-byte address devices

--- dev/i2c_eeprom.c
+++ dev/i2c_eeprom.c
@@ -11,6 +11,14 @@
 #include "dev/bb_i2c.h"
 #include "dev/i2c_eeprom.h"
 
+int i2c_eeprom_create( struct i2c_eeprom_device *dev, struct i2c_bus *bus, uint8_t i2c_addr, int offset_bytes )
+{
+	dev->bus = bus;
+	dev->offset_bytes = offset_bytes;
+	dev->addr = i2c_addr;
+	return 0;
+}
+
 int i2c_eeprom_read(struct i2c_eeprom_device *dev, int offset, void *buf, int count)
 {
 	int i;
@@ -21,7 +29,10 @@ int i2c_eeprom_read(struct i2c_eeprom_device *dev, int offset, void *buf, int co
 		bb_i2c_stop(dev->bus);
 		return -1;
 	}
-	bb_i2c_put_byte(dev->bus, (offset >> 8) & 0xff);
+
+	if(dev->offset_bytes == 2)
+		bb_i2c_put_byte(dev->bus, (offset >> 8) & 0xff);
+	
 	bb_i2c_put_byte(dev->bus, offset & 0xff);
 	bb_i2c_repeat_start(dev->bus);
 	bb_i2c_put_byte(dev->bus, (dev->addr << 1) | 1);
@@ -48,7 +59,10 @@ int i2c_eeprom_write(struct i2c_eeprom_device *dev, int offset, void *buf, int c
 			bb_i2c_stop(dev->bus);
 			return -1;
 		}
-		bb_i2c_put_byte(dev->bus, (offset >> 8) & 0xff);
+
+		if(dev->offset_bytes == 2)
+			bb_i2c_put_byte(dev->bus, (offset >> 8) & 0xff);
+
 		bb_i2c_put_byte(dev->bus, offset & 0xff);
 		bb_i2c_put_byte(dev->bus, *cb++);
 		offset++;
@@ -75,7 +89,10 @@ int i2c_eeprom_erase(struct i2c_eeprom_device *dev, int offset, int count)
 			bb_i2c_stop(dev->bus);
 			return -1;
 		}
-		bb_i2c_put_byte(dev->bus, (offset >> 8) & 0xff);
+
+		if(dev->offset_bytes == 2)
+			bb_i2c_put_byte(dev->bus, (offset >> 8) & 0xff);
+
 		bb_i2c_put_byte(dev->bus, offset & 0xff);
 		bb_i2c_put_byte(dev->bus, 0xff);
 		offset++;
