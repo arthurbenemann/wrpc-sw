980e0a590e06a55b3ebe75168ede88945f95ed49
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-07-10 12:31:59 +0200

    shell: calibration command now allows to read/set all calibration parameters

--- shell/cmd_calib.c
+++ shell/cmd_calib.c
@@ -24,10 +24,32 @@ static int cmd_calibration(const char *args[])
 {
 	uint32_t trans;
 
-	if (args[0] && !strcasecmp(args[0], "force")) {
-		if (measure_t24p(&trans) < 0)
-			return -1;
-		return storage_phtrans(&trans, 1);
+	if (args[0])
+	{
+		if(!strcasecmp(args[0], "force")) 
+		{
+			if (measure_t24p(&trans) < 0)
+				return -1;
+			return storage_phtrans(&trans, 1);
+		}
+		else if (!strcasecmp( args[0], "load" ) )
+		{
+			storage_load_calibration();
+		}
+		else if (!strcasecmp( args[0], "setp" ) )
+		{
+			uint32_t param = 0;
+			param |= ((uint32_t)(args[1][0])) << 24;
+			param |= ((uint32_t)(args[1][1])) << 16;
+			param |= ((uint32_t)(args[1][2])) << 8;
+			param |= ((uint32_t)(args[1][3])) << 0;
+
+			int value = atoi(args[2]);
+
+			pp_printf("Setting calibration parameter %s [0x%x] to %d\n", args[1], param ,value );
+			storage_set_calibration_parameter( param, value );
+		}
+
 	} else if (!args[0]) {
 		if (storage_phtrans(&trans, 0) > 0) {
 			pp_printf("Found phase transition in EEPROM: %dps\n",
