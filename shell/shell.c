0824635720ee9651bffe024bf75ce4660cf17e89
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-07-10 12:31:39 +0200

    shell: re-enable stat command

--- shell/shell.c
+++ shell/shell.c
@@ -364,6 +364,7 @@ void shell_register_commands(void)
 	REGISTER_WRC_COMMAND(diag);
 	REGISTER_WRC_COMMAND(init);
 	REGISTER_WRC_COMMAND(sfp);
+	REGISTER_WRC_COMMAND(stat);
 	if (HAS_IP)
 		REGISTER_WRC_COMMAND(ip);
 }
