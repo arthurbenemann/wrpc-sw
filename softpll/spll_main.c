7c23602e3948b3b681f68d026bbc361ee0f19c6c
guidov <guidov@nikhef.nl>
2020-09-10 12:49:06 +0200

    removed block parameter

--- softpll/spll_main.c
+++ softpll/spll_main.c
@@ -50,8 +50,6 @@ void mpll_init(struct spll_main_state *s, int id_ref,
 	s->pi.kp    = -4000 * 16;;		// / 2;
 	s->pi.ki    = -5 * 16;;			// / 2;
 	s->pi.shift = 20;
- 	s->pi.setblock=0;
-	
 #else
 #error "Please set CONFIG for wr switch or wr node"
 #endif
