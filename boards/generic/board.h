9eba2d02f3860b8810634d2f93c5d8a33508edc7
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2020-09-29 15:43:58 +0200

    boards/generic: update memory map after DPRAM space was increased to 256kB

--- boards/generic/board.h
+++ boards/generic/board.h
@@ -11,14 +11,14 @@
  */
 
 /* Fixed base addresses */
-#define BASE_MINIC	0x20000
-#define BASE_EP		0x20100
-#define BASE_SOFTPLL 	0x20200
-#define BASE_PPS_GEN 	0x20300
-#define BASE_SYSCON	0x20400
-#define BASE_UART	0x20500
-#define BASE_ONEWIRE	0x20600
-#define BASE_ETHERBONE_CFG	0x20700
+#define BASE_MINIC	0x40000
+#define BASE_EP		0x40100
+#define BASE_SOFTPLL 	0x40200
+#define BASE_PPS_GEN 	0x40300
+#define BASE_SYSCON	0x40400
+#define BASE_UART	0x40500
+#define BASE_ONEWIRE	0x40600
+#define BASE_ETHERBONE_CFG	0x48000
 
 /* Board-specific parameters */
 #define TICS_PER_SECOND 1000
