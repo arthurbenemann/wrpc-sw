9f34bfffdf4a1289841558517b739d3aa61d9f2c
Adam Wujek <adam.wujek@creotech.pl>
2021-02-19 15:30:41 +0100

    boards: add REQUIRE_AUTOCONF_PPSI for ertm14
    Signed-off-by: Adam Wujek <adam.wujek@creotech.pl>

--- boards/boards.mk
+++ boards/boards.mk
@@ -6,6 +6,11 @@ obj-$(CONFIG_TARGET_SIS8300KU) += boards/sis8300ku/board.o
 obj-$(CONFIG_TARGET_ERTM14) += boards/ertm14/board.o boards/ertm14/ertm15_rf_distr.o boards/ertm14/phy_calibration.o boards/ertm14/rf_frame_transceiver.o  boards/ertm14/cmd_ertm14.o
 obj-$(CONFIG_TARGET_PXIE_FMC) += boards/pxie-fmc/board.o
 
+REQUIRE_AUTOCONF_PPSI+= \
+			boards/ertm14/board.o \
+			boards/ertm14/cmd_ertm14.o \
+
+
 #boards/sis8300ku/board.o: boards/sis8300ku/sdbfs-image.h
 		#./tools/gensdbfs -c boards/sis8300ku/sdbfs-image.h boards/sis8300ku/sdbfs boards/sis8300ku/sdbfs-image.bin
 
