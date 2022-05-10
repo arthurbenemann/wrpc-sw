bb534f8cde9f7518314cd6ecdddce08c1dbbfec4
Vincent van Beveren <v.van.beveren@nikhef.nl>
2022-05-10 12:04:48 +0200

    disalbe autoneg

--- wrc_main.c
+++ wrc_main.c
@@ -97,7 +97,7 @@ static void wrc_initialize(void)
 	/* Sleep for 1s to make sure WRS v4.2 always realizes that
 	 * the link is down */
 	timer_delay_ms(200);
-	ep_enable(1, 1);
+	ep_enable(1, 0);
 
 	minic_init();
 	shw_pps_gen_init();
