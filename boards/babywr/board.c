60f1f2f7015e40f18ffe833d2a4564804e248f25
Peter Jansweijer <peterj@nikhef.nl>
2022-12-07 11:44:26 +0100

    add sit5359 interface.

--- boards/babywr/board.c
+++ boards/babywr/board.c
@@ -29,8 +29,132 @@
 #include "dev/endpoint.h"
 #include "storage.h"
 #include "softpll_ng.h"
+#include "hw/sit5359_regs.h"
 
-struct babywr_board board;
+//struct babywr_board board;
+
+struct wr_sit5359_interface_device
+{
+    void *base_addr;
+    uint8_t i2c_addr;
+    struct gpio_pin pin_scl;
+    struct gpio_pin pin_sda;
+    struct gpio_device gpio_i2c;
+    struct i2c_bus master;
+    int pull_range, hsdiv;
+    uint64_t rfreq;
+};
+
+#define SIT5359_PIN_SCL 0
+#define SIT5359_PIN_SDA 1
+
+// I2C_ADDR (A0=0) => 1100010 (WR:0xC4, RD:0xC5 or 0x62+r/w)
+// I2C_ADDR (A0=1) => 1101010 (WR:0xD4, RD:0xD5 or 0x6A+r/w)
+#define SIT5359_I2C_ADDR_A0_0 0x62
+#define SIT5359_I2C_ADDR_A0_1 0x6A
+
+struct
+{
+    struct gpio_device gpio_aux;
+    struct wr_sit5359_interface_device sit5359_refclk;
+    struct wr_sit5359_interface_device sit5359_dmtd;
+} board;
+
+static void sit5359_gpio_out(const struct gpio_pin *pin, int value)
+{
+    struct wr_sit5359_interface_device* dev = ( struct wr_sit5359_interface_device* ) pin->device->priv;
+
+    uint32_t mask = (pin->pin == SIT5359_PIN_SCL ? SIT5359_GPCR_SCL : SIT5359_GPCR_SDA );
+    uint32_t reg = (value ? SIT5359_REG_GPSR : SIT5359_REG_GPCR );
+
+    writel( mask, dev->base_addr + reg );
+}
+
+static void sit5359_gpio_set_dir(const struct gpio_pin *pin, int dir)
+{
+    sit5359_gpio_out(pin, !dir);
+}
+
+static int sit5359_gpio_in(const struct gpio_pin *pin)
+{
+    struct wr_sit5359_interface_device* dev = ( struct wr_sit5359_interface_device* ) pin->device->priv;
+
+    uint32_t gpsr = readl( dev->base_addr + SIT5359_REG_GPSR );
+
+    if ( pin->pin == SIT5359_PIN_SCL )
+        return (gpsr & SIT5359_GPSR_SCL ? 1 : 0);
+    else
+        return (gpsr & SIT5359_GPSR_SDA ? 1 : 0);
+}
+
+static void wr_sit5359_interface_init( struct wr_sit5359_interface_device *dev, uint32_t base_addr, uint8_t i2c_addr )
+{
+    int attempts = 0;
+
+    dev->base_addr = (void *) base_addr;
+    dev->gpio_i2c.priv = (void *) dev;
+    dev->gpio_i2c.read_pin = sit5359_gpio_in;
+    dev->gpio_i2c.set_dir = sit5359_gpio_set_dir;
+    dev->gpio_i2c.set_out = sit5359_gpio_out;
+    dev->i2c_addr = i2c_addr;
+    dev->pin_scl.device = &dev->gpio_i2c;
+    dev->pin_scl.pin = SIT5359_PIN_SCL;
+    dev->pin_sda.device = &dev->gpio_i2c;
+    dev->pin_sda.pin = SIT5359_PIN_SDA;
+    bb_i2c_create( &dev->master, &dev->pin_scl, &dev->pin_sda );
+    bb_i2c_init(&dev->master);
+
+    // Avoid failing initialization due to any pending I2C transfer from the past.
+    // Probe the SiTime oscillators to align their I2C state machine.
+    // Wait for a proper answer...
+    while (!bb_i2c_devprobe(&dev->master, i2c_addr)) {
+        attempts++;
+        if (attempts > 10) {
+            board_dbg("SiT5359 device not found.\n");
+            break;
+        }
+    }
+}
+
+static void sit5359_read( struct wr_sit5359_interface_device *dev, uint8_t addr, uint8_t *data, int count )
+{
+    int i;
+
+    bb_i2c_start( &dev->master );
+    bb_i2c_put_byte( &dev->master, dev->i2c_addr << 1 );
+    bb_i2c_put_byte( &dev->master, addr );
+    bb_i2c_repeat_start( &dev->master );
+    bb_i2c_put_byte( &dev->master, (dev->i2c_addr << 1) | 1 );
+
+    for(i = 0; i < count; i ++)
+        bb_i2c_get_byte( &dev->master, &data[i], i == (count - 1) ? 1 : 0 );
+
+    bb_i2c_stop( &dev->master );
+}
+
+static void sit5359_i2c_write( struct wr_sit5359_interface_device *dev, uint8_t addr, uint8_t *data, int count )
+{
+    int i;
+
+    bb_i2c_start( &dev->master );
+    bb_i2c_put_byte( &dev->master, dev->i2c_addr << 1 );
+    bb_i2c_put_byte( &dev->master, addr );
+
+    for(i = 0; i < count; i ++)
+    {
+        bb_i2c_put_byte( &dev->master, data[i] );
+    }
+
+    bb_i2c_stop( &dev->master );
+}
+
+static int sit5359_dev_init( struct wr_sit5359_interface_device *dev )
+{
+    // Enable SPLL and Osc Output Enable
+    // I2C bus freqency = 1/(4*(30+1)*16ns) = 504 KHz
+    writel( SIT5359_CR_SPLL_EN | SIT5359_CR_OSC_OE | SIT5359_CR_CLK_DIV_W(30) | SIT5359_CR_I2C_ADDR_W ( ( dev->i2c_addr << 1 ) ), dev->base_addr + SIT5359_REG_CR );
+    return 0;
+}
 
 static struct gpio_pin pin_eeprom_scl        = { &board.gpio_aux, 0 };
 static struct gpio_pin pin_eeprom_sda        = { &board.gpio_aux, 1 };
@@ -79,11 +203,37 @@ int wrc_board_early_init()
     /* create and initialize UID eeprom I2C bus */
     i2c_eeprom_create(&wrc_uid_dev, &i2c_wrc_eeprom, UID_EEPROM_ADR, 1);
 
+    wr_sit5359_interface_init( &board.sit5359_refclk, BASE_SIT5359_REFCLK, SIT5359_I2C_ADDR_A0_1 );
+    wr_sit5359_interface_init( &board.sit5359_dmtd, BASE_SIT5359_DMTD, SIT5359_I2C_ADDR_A0_0 );
+
     return 0;
 }
 
 int wrc_board_init()
 {
+    uint8_t regs[6];
+
+    // set I2C bus speed and OSC Output enable
+    sit5359_dev_init(&board.sit5359_refclk);
+    sit5359_dev_init(&board.sit5359_dmtd);
+
+    regs[0] = 0x00; // SiT5339 Reg 0x00 15:8 => DFC-LSW[15:8]
+    regs[1] = 0x00; // SiT5339 Reg 0x00 7:0  => DFC-LSW[7:0]
+    regs[2] = 0x00; // SiT5339 Reg 0x01 15:8 => DFC-MSW[9:8] (PartNo option "I": hardware OE via pin 1)
+    regs[3] = 0x00; // SiT5339 Reg 0x01 7:0  => DFC-MSW[7:0]
+    regs[4] = 0x00; // SiT5339 Reg 0x02 15:8 => not used
+    regs[5] = 0x03; // SiT5339 Reg 0x02 7:0  => Pull Range 25 ppm
+    // write SiT5359 0x02 with Pull Range (2 bytes)
+    sit5359_i2c_write(&board.sit5359_refclk, 0x00, regs, 6 );
+    sit5359_i2c_write(&board.sit5359_dmtd, 0x00, regs, 6 );
+
+    // read back all SiT5359_refclk registers (6 bytes)
+    sit5359_read(&board.sit5359_refclk, 0x00, regs, 6 );
+    board_dbg("SiT5359 RefClk Regs: %x:%x:%x:%x:%x:%x\n",regs[0],regs[1],regs[2],regs[3],regs[4],regs[5]);
+    // read back all SiT5359_dmtd registers (6 bytes)
+    sit5359_read(&board.sit5359_dmtd, 0x00, regs, 6 );
+    board_dbg("SiT5359 DMTD Regs: %x:%x:%x:%x:%x:%x\n",regs[0],regs[1],regs[2],regs[3],regs[4],regs[5]);
+
     uint8_t mac_addr[6];
     /*
      * Read MAC addr from Unique-ID, IC D12, 24AA025E48
