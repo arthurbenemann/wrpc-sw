daf4ec932fb7fffff721d35fcd3e6e085c38ad91
Tristan Gingold <tristan.gingold@cern.ch>
2023-04-24 14:20:27 +0200

    bootloader: replace Makefile with boot.mk

--- Makefile
+++ Makefile
@@ -57,14 +57,6 @@ LDS-$(CONFIG_ARCH_RISCV)  = arch/risc-v/ram.ld
 LDS-$(CONFIG_TARGET_WR_SWITCH) = arch/lm32/ram-wrs.ld
 LDS-$(CONFIG_HOST_PROCESS) =
 
-# Add bootloader
-obj-$(CONFIG_BOOTLOADER) += tools/uart-bootloader/bootloader.o
-ifeq ($(CONFIG_BOOTLOADER),m)
-# Just in case: allow to build a version without a bootloader just with a
-# simple link
-obj-$(CONFIG_ARCH_RISCV) += arch/risc-v/no-bootloader.o
-endif
-
 obj-$(CONFIG_WR_NODE)   += wrc_main.o
 obj-$(CONFIG_WR_NODE_SIM) += wrc_main_sim.o
 obj-$(CONFIG_TARGET_WR_SWITCH) += ipc/minipc-mem-server.o ipc/rt_ipc.o
@@ -113,6 +105,7 @@ ldflags-$(CONFIG_ARCH_LM32) = -mmultiply-enabled -mbarrel-shift-enabled \
 ldflags-$(CONFIG_ARCH_RISCV) = -march=rv32im$(USE-COMP-INSTR-y) -mabi=ilp32 \
 	-nostdlib -T $(LDS-y)
 asflags-$(CONFIG_ARCH_RISCV) += -march=rv32im$(USE-COMP-INSTR-y)_zicsr -mabi=ilp32
+
 arch-files-$(CONFIG_ARCH_LM32) = $(OUTPUT).bram $(OUTPUT).vhd $(OUTPUT).mif
 arch-files-$(CONFIG_ARCH_RISCV) = $(OUTPUT).bram $(OUTPUT).vhd $(OUTPUT).mif
 
@@ -138,6 +131,7 @@ include pp_printf/printf.mk
 include dev/dev.mk
 include softpll/softpll.mk
 include host/host.mk
+include tools/uart-bootloader/boot.mk
 
 # ppsi already has div64 (the same one), so only pick it if not using ppsi.
 ifndef CONFIG_WRPC_PPSI
@@ -146,10 +140,6 @@ endif
 # And always complain if we pick the libgcc division: 64/32 = 32 is enough here.
 obj-$(CONFIG_ARCH_LM32)  += check-error.o
 obj-$(CONFIG_ARCH_RISCV) += check-error.o
-
-
-tools/uart-bootloader/bootloader.o:
-	$(MAKE) -C tools/uart-bootloader bootloader.o
 
 # add system check functions like stack overflow and check reset
 obj-y += system_checks.o
--- tools/uart-bootloader/boot.mk
+++ tools/uart-bootloader/boot.mk
@@ -1,57 +1,45 @@
-# and don't touch the rest unless you know what you're doing.
-CROSS_COMPILE ?= riscv32-elf-
+BOOTLOADER_CFLAGS = $(CFLAGS) -DIN_BOOTLOADER
 
-CC =		$(CROSS_COMPILE)gcc
-LD =		$(CROSS_COMPILE)ld
-OBJDUMP =	$(CROSS_COMPILE)objdump
-OBJCOPY =	$(CROSS_COMPILE)objcopy
-SIZE =		$(CROSS_COMPILE)size
+BOOTLOADER_OBJS = \
+ tools/uart-bootloader/boot-crt0.o \
+ tools/uart-bootloader/boot.o \
+ tools/uart-bootloader/timer.o \
+ tools/uart-bootloader/bb_spi.o \
+ tools/uart-bootloader/gpio.o \
+ tools/uart-bootloader/spi_flash.o \
+ tools/uart-bootloader/simple_uart.o \
+ tools/uart-bootloader/div64.o
 
-AUTOCONF ?= ../../include/generated/autoconf.h
+BOOTLOADER_LDFLAGS =  -Os -march=rv32im -mabi=ilp32
 
-all: bootloader.o
-
-CFLAGS = -include $(AUTOCONF) -I../../ -Os -I../common -I.  -march=rv32im -mabi=ilp32 -I../../include -DCONFIG_TARGET_ERTM14 -I../../pp_printf -DIN_BOOTLOADER
-OBJS = boot-crt0.o boot.o timer.o bb_spi.o gpio.o spi_flash.o  simple_uart.o div64.o
-LDS = ../../arch/risc-v/boot.ld
-LDFLAGS =  -Os -march=rv32im -mabi=ilp32
-OUTPUT=wrc-bootloader
-BRAM_IMAGE=../../wrc-bootloader.ram
-
-%.o:	%.c
-	${CC} $(CFLAGS) -c $^ -o $@
-
-%.o:	%.S
-	${CC} $(CFLAGS) -c $^ -o $@
-
-bootloader.o: $(OBJS)
-	${CC} -o bootloader-tmp.o -nostartfiles $(LDFLAGS) -r $(OBJS) -lgcc -lc -e _bootloader_entry -Wl,-Map,bootloader.map
+tools/uart-bootloader/bootloader.o: $(BOOTLOADER_OBJS)
+	${CC} -o bootloader-tmp.o -nostartfiles $(BOOTLOADER_LDFLAGS) -r $(BOOTLOADER_OBJS) -lgcc -lc -e _bootloader_entry -Wl,-Map,bootloader.map
 	${OBJCOPY} -G _bootloader_entry --prefix-alloc-section loader bootloader-tmp.o $@
 
-boot-crt0.o: ../../arch/risc-v/boot-crt0.S
-	${CC} $(CFLAGS) -c $^ -o $@
+tools/uart-bootloader/boot-crt0.o: arch/risc-v/boot-crt0.S
+	${CC} $(BOOTLOADER_CFLAGS) -c $^ -o $@
 
-bb_spi.o: ../../dev/bb_spi.c
-	${CC} $(CFLAGS) -c $^ -o $@
+tools/uart-bootloader/bb_spi.o: dev/bb_spi.c
+	${CC} $(BOOTLOADER_CFLAGS) -c $^ -o $@
 
-gpio.o: ../../dev/gpio.c
-	${CC} $(CFLAGS) -c $^ -o $@
+tools/uart-bootloader/gpio.o: dev/gpio.c
+	${CC} $(BOOTLOADER_CFLAGS) -c $^ -o $@
 
-spi_flash.o: ../../dev/spi_flash.c
-	${CC} $(CFLAGS) -c $^ -o $@
+tools/uart-bootloader/spi_flash.o: dev/spi_flash.c
+	${CC} $(BOOTLOADER_CFLAGS) -c $^ -o $@
 
-simple_uart.o: ../../dev/simple_uart.c
-	${CC} $(CFLAGS) -c $^ -o $@
+tools/uart-bootloader/simple_uart.o: dev/simple_uart.c
+	${CC} $(BOOTLOADER_CFLAGS) -c $^ -o $@
 
-div64.o: ../../pp_printf/div64.c
-	${CC} $(CFLAGS) -c $^ -o $@
+tools/uart-bootloader/div64.o: pp_printf/div64.c
+	${CC} $(BOOTLOADER_CFLAGS) -c $^ -o $@
 
-$(OUTPUT): $(LDS) $(OBJS)
-	${CC} -o $(OUTPUT).elf -nostartfiles $(LDFLAGS) $(OBJS) -T $(LDS) -lgcc -lc
-	${OBJCOPY} -O binary $(OUTPUT).elf $(OUTPUT).bin
-	${OBJDUMP} -D $(OUTPUT).elf > disasm.S
-	$(SIZE) $(OUTPUT).elf
-	../genraminit -l $(OUTPUT).bin 32768 > $(BRAM_IMAGE)
 
-clean:
-	rm -f $(OBJS) $(OUTPUT).bin bootloader.o bootloader.map
+obj-$(CONFIG_BOOTLOADER) += tools/uart-bootloader/bootloader.o
+
+ifneq ($(CONFIG_BOOTLOADER),y)
+# Just in case: allow to build a version without a bootloader just with a
+# simple link
+obj-$(CONFIG_ARCH_RISCV) += arch/risc-v/no-bootloader.o
+endif
+
