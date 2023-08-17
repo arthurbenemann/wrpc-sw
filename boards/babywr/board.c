9380eccaee94cd45ac8281bbbb40dcfb2d8486c0
Peter Jansweijer <peterj@nikhef.nl>
2023-08-17 11:22:19 +0200

    mpll_restart function for lock_sweep

--- boards/babywr/board.c
+++ boards/babywr/board.c
@@ -258,6 +258,13 @@ int lock_sweep()
   return gen_gpio_in( &pin_lock_sweep );
 }
 
+void mpll_restart()
+{
+	struct softpll_state *s = (struct softpll_state *)&softpll;
+	mpll_stop(&s->mpll);
+	mpll_start(&s->mpll);
+}
+
 int wrc_board_early_init()
 {
     /* most of the I/Os of the slow peripherals (i2c, spi) are bitbanged. First, let's
--- boards/babywr/board.h
+++ boards/babywr/board.h
@@ -81,6 +81,7 @@ int  babywr_init(void);
 void read_sitime (void);
 void write_sitime (int dev, int val);
 int  lock_sweep(void);
+void mpll_restart(void);
 
 extern int phy_calibration_poll(void);
 extern void phy_calibration_init(void);
