8bc94cc2236e0e9f2e5e4ff1e008fa5abe34e036
Harvey Leicester <harvey.macdonald.leicester@cern.ch>
2024-07-05 13:52:39 +0200

    added configs for wrsv4 and wrsv4 on afcz

---
+++ boards/wr-switch-v4-on-afcz/gpio-wrs.h
@@ -0,0 +1,26 @@
+/*
+ * This work is part of the White Rabbit project
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+#ifndef __GPIO_WRS_H
+#define __GPIO_WRS_H
+
+#include "dev/gpio.h"
+
+extern const struct gpio_pin gpio_pin_sys_clk_sel;
+extern const struct gpio_pin gpio_pin_pll_reset_n;
+extern const struct gpio_pin gpio_pin_periph_reset_n;
+extern const struct gpio_pin gpio_pin_ljd_board_detect;
+extern const struct gpio_pin gpio_pin_ep_reset_n;
+
+extern const struct gpio_pin gpio_pin_pll_clk_sel;
+extern const struct gpio_pin gpio_pin_pll_sync;
+extern const struct gpio_pin gpio_pin_pll_gpio1;
+extern const struct gpio_pin gpio_pin_pll_gpio2;
+extern const struct gpio_pin gpio_pin_gm_pll_muxout_ld;
+extern const struct gpio_pin gpio_pin_gm_pll_sync;
+
+void wrs_gpio_init(void);
+
+#endif
