60e4cdfb37ec75313ccf0b03862db77a757c8cf6
Adam Wujek <dev_public@wujek.eu>
2024-07-19 16:53:04 +0200

    WRSv7.0: Change ad95 config
    not-Signed-off-by: Adam Wujek <dev_public@wujek.eu>

--- wrs_main.c
+++ wrs_main.c
@@ -82,7 +82,7 @@ int main(void)
 	pp_printf("SCB version: %d. %s\n", scb_ver,(scb_ver>=34)?"10 MHz SMC Output.":"" );
 	pp_printf("Start counter %d\n", stats.start_cnt);
 	/* Low-jitter Daughterboard detection */
-	ljd_present = gpio_in(GPIO_LJD_BOARD_DETECT);
+	ljd_present = 1;
 	lj_periph_type = lj_periph_type_read(ljd_present);
 
 	if (stats.start_cnt > 1) {
