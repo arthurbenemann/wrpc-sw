6ecbf94f9af804e9120f270b3011566b91488162
Harvey Leicester <harvey.macdonald.leicester@cern.ch>
2024-10-17 09:50:31 +0200

    Revert "add spll pi gains as config options"
    This reverts commit c918e0f37edd6b82c8f9bca50ff5186c2a76959a.

--- softpll/spll_helper.c
+++ softpll/spll_helper.c
@@ -20,13 +20,8 @@ void helper_very_init( struct spll_helper_state *s )
 	s->pi.kp = -150;
 	s->pi.ki = -2;
 #else
-	#if defined(CONFIG_SPLL_PI_SETTINGS)
-		s->pi.kp = CONFIG_HPLL_KP;
-		s->pi.ki = CONFIG_HPLL_KI;
-	#else
-		s->pi.kp = 150;
-		s->pi.ki = 2;
-	#endif
+	s->pi.kp = 150;
+	s->pi.ki = 2;
 #endif
 	s->pi.shift = PI_FRACBITS - BOARD_SPLL_DIV_BITS;
 	s->pi.anti_windup = 1;
