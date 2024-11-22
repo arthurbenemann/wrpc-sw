b91b8b3d56a295aa34c5b60dbece8ade3e246495
Adam Wujek <dev_public@wujek.eu>
2024-11-22 02:33:45 +0100

    softpll: move LJ capabilities definitions from softpll_ng.h to softpll_export.h
    No technical change.
    
    Signed-off-by: Adam Wujek <dev_public@wujek.eu>

--- softpll/softpll_export.h
+++ softpll/softpll_export.h
@@ -65,6 +65,30 @@
 #define SPLL_LOOP_HELPER -1
 #define SPLL_LOOP_MAIN 0
 
+/*
+ * Low Jitter capabilities corner
+ */
+#define PERIPH_WRS_STD_NO_LJ    0
+#define PERIPH_WRS_STD_WITH_LJD 1
+#define PERIPH_WRS_FL_SYNCTECH  2
+#define PERIPH_WRS_LJ_SAFRAN    3
+#define PERIPH_END              4
+
+/* OSC_FREQ allows to recognize frequency of oscillator on LJD and
+ * also the fact that LJ functionality is intgrated */
+#define OSC_FREQ_10M        0x0
+#define OSC_FREQ_20M        0x1
+#define OSC_FREQ_25M        0x2
+#define OSC_FREQ_50M        0x3
+#define OSC_FREQ_100M       0x4
+#define OSC_FREQ_WRS_LJ_INT 0x7
+
+/* When Low Jitter functionlity integrated, periph_ID allows to detect
+ * the switch type */
+#define PERIPH_ID_WRS_FL_SYNCTECHv1_0 0x7
+#define PERIPH_ID_WRS_FL_SYNCTECHv1_5 0x6
+#define PERIPH_ID_WRS_LJ_SAFRAN       0x5
+
 struct spll_build_id {
 	char commit_id[32];
 	char build_date[16];
--- softpll/softpll_ng.h
+++ softpll/softpll_ng.h
@@ -181,32 +181,6 @@ struct softpll_state {
 	struct spll_ptracker_state ptrackers[MAX_PTRACKERS];
 };
 
-/*
- * Low Jitter capabilities corner
- */
-
-#define PERIPH_WRS_STD_NO_LJ    0
-#define PERIPH_WRS_STD_WITH_LJD 1
-#define PERIPH_WRS_FL_SYNCTECH  2
-#define PERIPH_WRS_LJ_SAFRAN    3
-#define PERIPH_END              4
-
-// OSC_FREQ allows to recognize frequency of oscillator on LJD and
-// also the fact that LJ functionality is intgrated
-#define OSC_FREQ_10M        0x0
-#define OSC_FREQ_20M        0x1
-#define OSC_FREQ_25M        0x2
-#define OSC_FREQ_50M        0x3
-#define OSC_FREQ_100M       0x4
-#define OSC_FREQ_WRS_LJ_INT 0x7
-
-// When Low Jitter functionlity integrated, periph_ID allows to detect
-// the switch type
-#define PERIPH_ID_WRS_FL_SYNCTECHv1_0 0x7
-#define PERIPH_ID_WRS_FL_SYNCTECHv1_5 0x6
-#define PERIPH_ID_WRS_LJ_SAFRAN       0x5
-
-
 extern volatile struct softpll_state softpll;
 extern int lj_periph_type;
 extern int scb_ljd_present_global;
